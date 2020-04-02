9f3bd180f263db1a1f7aa91f9bfbffd1789ca238
jagiello430 <jagiello430@o2.pl>
2020-04-02 15:59:45 +0000

    REST API

---
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -0,0 +1,14 @@
+package com.crud.tasks;
+
+import com.crud.tasks.domain.TaskDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
