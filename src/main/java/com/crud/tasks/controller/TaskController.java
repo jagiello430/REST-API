0322cd61ace382c05c76895550837e3592172bfd
jagiello430 <jagiello430@o2.pl>
2020-04-06 09:32:35 +0000

    rest api

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,13 +1,16 @@
 package com.crud.tasks.controller;
 
-import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
 
 @RestController
 @RequestMapping("/v1/task")
@@ -23,20 +26,22 @@ public class TaskController {
     }
 
     @GetMapping(value = "getTask")
-    public Task getTask(Long taskId) {
-        return dbService.getTaskById(taskId);
+    public TaskDto getTask(@RequestParam Long taskId) throws TaskNotFoundException {
+        return taskMapper.mapToTaskDto(dbService.getTaskById(taskId).orElseThrow(TaskNotFoundException::new));
     }
 
     @DeleteMapping(value = "deleteTask")
-    public void deleteTask(Long taskId) {
+    public void deleteTask(@RequestParam Long taskId) {
+        taskMapper.mapToTaskDto(dbService.deleteTask(taskId));
     }
 
     @PutMapping(value = "updateTask")
     public TaskDto updateTask(TaskDto task) {
-        return new TaskDto(1L, "Edited test title", "test content");
+        return taskMapper.mapToTaskDto(dbService.saveTask(taskMapper.mapToTask(task)));
     }
 
-    @PostMapping(value = "createTask")
-    public void createTask(TaskDto task) {
+    @PostMapping(value = "createTask", consumes = APPLICATION_JSON_VALUE)
+    public void createTask(@RequestBody TaskDto taskDto) {
+        dbService.saveTask(taskMapper.mapToTask(taskDto));
     }
 }
--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -3,6 +3,7 @@ package com.crud.tasks.repository;
 import com.crud.tasks.domain.Task;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +12,12 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
 
+    @Override
+    Task save(Task task);
+
     @Query
-    Optional<Task> findById(long id);
+    Optional<Task> findById(@Param("Id") long id);
+
+    @Query
+    Task deleteTaskById(@Param("Id") long id);
 }
